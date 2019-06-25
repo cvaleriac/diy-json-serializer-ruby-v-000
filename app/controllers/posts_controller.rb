896022e04f0e6833c99d0ba291a921ff16273173
valeria campilongo <cvaleriac@hotmail.it>
2019-06-25 19:34:31 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,15 +26,11 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def body
+  def post_data
     post = Post.find(params[:id])
     render json: PostSerializer.serialize(post)
   end
 
-  def post_data
-    post = Post.find(params[:id])
-    render plain: post.description
-  end
 
 private
   # Use callbacks to share common setup or constraints between actions.
